a8ddc33954876d438ed64d74291b4ad80a316f65
Saransh Mittal <saransh.mittal1998@gmail.com>
2020-04-09 17:28:06 +0530

    Added auth functions to create_user and get_user_keys

---
+++ src/utils/auth.py
@@ -0,0 +1,32 @@
+import base64
+from src.utils.ipfs import add_json, get_json
+from src.utils.cryptography import generate_keys, encrypt_object, decrypt_object
+
+def create_user():
+    private_key, public_key = generate_keys() # str object
+    encrypted_private_key, password = encrypt_object(private_key) # byte object
+    print(encrypted_private_key)
+    object = {
+        "private-key": encrypted_private_key,
+        "public-key": public_key
+    }
+    hash, status = add_json(object)
+    return {
+        "generated-password": password,
+        "user-hash": hash,
+        "ipfs-object": object
+    }
+
+def get_user_keys(hash, password):
+    object, status = get_json(hash)
+    public_key = object['public-key']
+    encrypted_private_key = object['private-key']
+    decrypted_private_key = decrypt_object(encrypted_private_key, password)
+    object = {
+        "private-key": decrypted_private_key,
+        "public-key": public_key
+    }
+    return {
+        "user-hash": hash,
+        "ipfs-object": object
+    }
