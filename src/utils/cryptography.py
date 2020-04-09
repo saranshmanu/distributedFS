d9b9bc4a9eb838153d599ec3636f818d476f498d
Saransh Mittal <saransh.mittal1998@gmail.com>
2020-04-09 17:27:34 +0530

    Added functions to create_hash, encrypt and decrypt string

--- src/utils/cryptography.py
+++ src/utils/cryptography.py
@@ -1,5 +1,8 @@
+from cryptography.fernet import Fernet
+from Crypto import Random
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
+from Crypto.Hash import BLAKE2s
 from binascii import hexlify
 
 # generate public_key and private_key for assymetric key encryption and decryption
@@ -25,3 +28,21 @@ def decrypt_file(data, private_key):
     decrypt = PKCS1_OAEP.new(key=private_key)
     decrypted_data = decrypt.decrypt(data)
     return decrypted_data
+
+def create_hash(text):
+    h_obj = BLAKE2s.new(digest_bits=256)
+    h_obj.update(text.encode())
+    return h_obj.hexdigest()
+ 
+# encrypt data using key
+def encrypt_object(message):
+    key = Fernet.generate_key()
+    cipher_suite = Fernet(key)
+    encrypted = cipher_suite.encrypt(message.encode())
+    return encrypted.decode(), key.decode()
+
+# decrypt data using key
+def decrypt_object(message, key):
+    cipher_suite = Fernet(key.encode())
+    decrypted = cipher_suite.decrypt(message.encode())
+    return decrypted.decode()
